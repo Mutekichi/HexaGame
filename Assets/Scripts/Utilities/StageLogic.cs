4b2c8b0684c0d378e49148d59ac6ed52494febcf
Mutekichi <ppwatanabe19568@gmail.com>
2024-10-22 19:36:11 +0900

    fix: 文字列からの盤面生成について、文字列通りの向きの盤面が生成されるようにした

--- Assets/Scripts/Utilities/StageLogic.cs
+++ Assets/Scripts/Utilities/StageLogic.cs
@@ -1,70 +1,51 @@
 using System;
+using System.Collections;
+using System.Linq;
 using UnityEngine;
 
-public class StageLogic
+public class StageLogic: MonoBehaviour
 {
-    public static void Test()
+    void Start()
     {
-        string[] stringExpression = new string[] {
-            "0121210",
-            "2121212",
-            "1212121",
-            "0212120",
-        };
-
-        int sampleHeight = 4;
-        int sampleWidth = 7;
-
-        CellState[,] cellStateStrings = GetCellStateStringsFromStringExpression(stringExpression, sampleHeight, sampleWidth);
-        CellExpression sampleCellExpression = new(sampleHeight, sampleWidth, true, cellStateStrings);
-
-        Board sampleBoard = CellExpression.GenerateBoard(sampleCellExpression);
-
-        Debug.Log("Board size: " + sampleBoard.size);
-        // as bitstring
-        Debug.Log("Board state: " + Convert.ToString(sampleBoard.boardState, 2));
-        Debug.Log("Board connectivity: ");
-        for (int i = 0; i < sampleBoard.size; ++i) {
-            Debug.Log("Tile " + i + " neighbors: " + sampleBoard.tiles[i].neighbors[0] + " " + sampleBoard.tiles[i].neighbors[1] + " " + sampleBoard.tiles[i].neighbors[2]);
-        }
+        Test();
     }
-
-
     public struct Stage 
     {
-        Board initialBoard;
-        Board currentBoard;
-        Board answerBoard;
-        int minMoves;
+        public Board initialBoard;
+        public Board currentBoard;
+        public Board answerBoard;
+        public int minMoves;
     }
+
     public struct Tile
     {
         public int index;
-        public bool isUpward; // true if the triangle is △ / ▲, false if the triangle is ▽ / ▼
-        public bool isFront; // true if the triangle is △ / ▽, true if the triangle is ▲ / ▼, false 
-        public int[] neighbors; // neighbors[0]: to the right, [1]: to the left, [2]: to the top or bottom    
+        public bool isUpward;
+        public bool isFront;
+        public int[] neighbors;
     }
-    // collection of tiles
+
     public struct Board 
     {
         public int size;
         public Tile[] tiles;
-        public int boardState; // bitstring of length size, boardState & (1 << i) == 1 if the i-th tile is front.
+        public BitArray boardState;
     }
+
     public enum CellState
     {
         Empty,
         Front,
         Back
     }
+
     public struct CellExpression
     {
         public int height;
         public int width;
-        public bool isTopLeftTriangleDownward; // true if the top left triangle is ▽ / ▼, false if the top left triangle is △ / ▲
+        public bool isTopLeftTriangleDownward;
         public CellState[,] cells;
 
-        // initializer
         public CellExpression(int height, int width, bool isTopLeftTriangleDownward, CellState[,] cells)
         {
             this.height = height;
@@ -84,75 +65,77 @@ public class StageLogic
 
             int size = CalcBoardSize(cells);
             Tile[] tiles = new Tile[size];
-
-            int boardState = 0;
+            BitArray boardState = new BitArray(size);
 
             int cnt = 0;
-            for (int ih = 0; ih < height; ++ih) {
-                for (int iw = 0; iw < width; ++iw) {
-                    if (cells[ih, iw] == CellState.Empty) {
+            for (int ih = 0; ih < height; ++ih)
+            {
+                for (int iw = 0; iw < width; ++iw)
+                {
+                    if (cells[height - 1 - ih, iw] == CellState.Empty)
+                    {
                         tileIndices[ih, iw] = -1;
-                    } else {
-                        tiles[cnt] = new Tile();
-                        tiles[cnt].index = cnt;
-                        tiles[cnt].isFront = cells[ih, iw] == CellState.Front;
-                        tiles[cnt].isUpward = isTopLeftTriangleDownward ? (ih + iw) % 2 == 0 : (ih + iw) % 2 == 1;
-                        if (tiles[cnt].isFront) {
-                            boardState += 1 << cnt;
-                        }
+                    }
+                    else
+                    {
+                        tiles[cnt] = new Tile
+                        {
+                            index = cnt,
+                            isFront = cells[height - 1 - ih, iw] == CellState.Front,
+                            isUpward = isTopLeftTriangleDownward ? (ih + iw) % 2 == 0 : (ih + iw) % 2 == 1,
+                            neighbors = new int[3] { -1, -1, -1 }
+                        };
+                        
+                        boardState[cnt] = tiles[cnt].isFront;
                         tileIndices[ih, iw] = cnt++;
                     }
                 }
             }
 
-            for (int ih = 0; ih < height; ++ih) {
-                for (int iw = 0; iw < width; ++iw) {
+            for (int ih = 0; ih < height; ++ih)
+            {
+                for (int iw = 0; iw < width; ++iw)
+                {
+                    if (tileIndices[ih, iw] == -1) continue;
 
-                    if (tileIndices[ih, iw] == -1) {
-                        continue;
+                    int currentIndex = tileIndices[ih, iw];
+
+                    if (iw < width - 1 && tileIndices[ih, iw + 1] != -1)
+                        tiles[currentIndex].neighbors[0] = tileIndices[ih, iw + 1];
+
+                    if (iw > 0 && tileIndices[ih, iw - 1] != -1)
+                        tiles[currentIndex].neighbors[1] = tileIndices[ih, iw - 1];
+
+                    if (IsDownwardTile(ih, iw, isTopLeftTriangleDownward))
+                    {
+                        if (ih > 0 && tileIndices[ih - 1, iw] != -1)
+                            tiles[currentIndex].neighbors[2] = tileIndices[ih - 1, iw];
                     }
-                    tiles[tileIndices[ih, iw]].neighbors = new int[3];
-                    if (iw != width - 1 && tileIndices[ih, iw + 1] != -1) {
-                        tiles[tileIndices[ih, iw]].neighbors[0] = tileIndices[ih, iw + 1];
-                    } else {
-                        tiles[tileIndices[ih, iw]].neighbors[0] = -1;
+                    else
+                    {
+                        if (ih < height - 1 && tileIndices[ih + 1, iw] != -1)
+                            tiles[currentIndex].neighbors[2] = tileIndices[ih + 1, iw];
                     }
-                    if (iw != 0 && tileIndices[ih, iw - 1] != -1) {
-                        tiles[tileIndices[ih, iw]].neighbors[1] = tileIndices[ih, iw - 1];
-                    } else {
-                        tiles[tileIndices[ih, iw]].neighbors[1] = -1;
-                    }
-                    if (ih != 0 && IsDownwardTile(ih, iw, isTopLeftTriangleDownward)) {
-                        if (tileIndices[ih - 1, iw] != -1) {
-                            tiles[tileIndices[ih, iw]].neighbors[2] = tileIndices[ih - 1, iw];
-                            continue;
-                        }
-                    }
-                    if (ih != height - 1 && !IsDownwardTile(ih, iw, isTopLeftTriangleDownward)) {
-                        if (tileIndices[ih + 1, iw] != -1) {
-                            tiles[tileIndices[ih, iw]].neighbors[2] = tileIndices[ih + 1, iw];
-                            continue;
-                        }
-                    }
-                    tiles[tileIndices[ih, iw]].neighbors[2] = -1;
                 }
             }
 
-            return new Board() {
+            return new Board
+            {
                 size = size,
                 tiles = tiles,
                 boardState = boardState
             };
         }
-        
 
-        
         private static int CalcBoardSize(CellState[,] board)
         {
             int size = 0;
-            for (int i = 0; i < board.GetLength(0); ++i) {
-                for (int j = 0; j < board.GetLength(1); ++j) {
-                    if (board[i, j] != CellState.Empty) {
+            for (int i = 0; i < board.GetLength(0); ++i)
+            {
+                for (int j = 0; j < board.GetLength(1); ++j)
+                {
+                    if (board[i, j] != CellState.Empty)
+                    {
                         size++;
                     }
                 }
@@ -161,13 +144,67 @@ public class StageLogic
         }
     }
 
+    public static class BoardHelper
+    {
+        public static string GetBoardStateString(Board board)
+        {
+            char[] bits = new char[board.size];
+            for (int i = 0; i < board.size; i++)
+            {
+                bits[i] = board.boardState[i] ? '1' : '0';
+            }
+            return new string(bits);
+        }
+
+        public static string GetFormattedBoardState(Board board, int groupSize = 4)
+        {
+            string binary = GetBoardStateString(board);
+            return string.Join("_", 
+                binary.Reverse()
+                    .Select((c, i) => new { c, i })
+                    .GroupBy(x => x.i / groupSize)
+                    .Select(g => new string(g.Select(x => x.c).Reverse().ToArray()))
+                    .Reverse());
+        }
+
+        public static string GetBoardVisualization(Board board)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.AppendLine($"Board Size: {board.size}");
+            sb.AppendLine($"Board State: {GetBoardStateString(board)}");
+            sb.AppendLine($"Formatted State: {GetFormattedBoardState(board)}");
+            sb.AppendLine("\nTiles:");
+            
+            for (int i = 0; i < board.size; i++)
+            {
+                Tile tile = board.tiles[i];
+                sb.AppendLine($"\nTile {i}:");
+                sb.AppendLine($"  Direction: {(tile.isUpward ? "△" : "▽")}");
+                sb.AppendLine($"  Face: {(tile.isFront ? "Front" : "Back")}");
+                sb.AppendLine($"  Neighbors:");
+                sb.AppendLine($"    Right: {FormatNeighbor(tile.neighbors[0])}");
+                sb.AppendLine($"    Left: {FormatNeighbor(tile.neighbors[1])}");
+                sb.AppendLine($"    {(tile.isUpward ? "Bottom" : "Top")}: {FormatNeighbor(tile.neighbors[2])}");
+            }
+            
+            return sb.ToString();
+        }
+
+        private static string FormatNeighbor(int neighbor)
+        {
+            return neighbor == -1 ? "none" : neighbor.ToString();
+        }
+    }
+
     public static CellState[,] GetCellStateStringsFromStringExpression(string[] stringExpression, int height, int width)
     {
-        // '0' for empty, '1' for front, '2' for back
         CellState[,] cellExpression = new CellState[height, width];
-        for (int i = 0; i < height; ++i) {
-            for (int j = 0; j < width; ++j) {
-                switch (stringExpression[i][j]) {
+        for (int i = 0; i < height; ++i)
+        {
+            for (int j = 0; j < width; ++j)
+            {
+                switch (stringExpression[i][j])
+                {
                     case '0':
                         cellExpression[i, j] = CellState.Empty;
                         break;
@@ -178,47 +215,42 @@ public class StageLogic
                         cellExpression[i, j] = CellState.Back;
                         break;
                     default:
-                        throw new ArgumentException("Invalid character in the string expression.");
+                        throw new ArgumentException($"Invalid character '{stringExpression[i][j]}' in the string expression at position ({i}, {j})");
                 }
             }
         }
         return cellExpression;
     }
+
     private static bool IsDownwardTile(int ih, int iw, bool isTopLeftTriangleDownward)
     {
         return isTopLeftTriangleDownward ? (ih + iw) % 2 == 0 : (ih + iw) % 2 == 1;
     }
-    private int GetBoardBitstring(int size, Tile[] tiles)
+
+    public static void Test()
     {
-        int bitstring = 0;
-        for (int i = 0; i < size; ++i) {
-            bitstring |= (tiles[i].isUpward ? 1 : 0) << (i);
-        }
+        string[] stringExpression = new string[] {
+            "0212120",
+            "1212121",
+            "2121212",
+            "0000010"
+        };
 
-        return bitstring;
-    }
+        int sampleHeight = 4;
+        int sampleWidth = 7;
 
-    private static int Popcount(int n)
-    {
-        int count = 0;
-        while (n != 0)
+        try
         {
-            n &= (n - 1);
-            count++;
-        }
-        return count;
-    }
+            CellState[,] cellStateStrings = GetCellStateStringsFromStringExpression(stringExpression, sampleHeight, sampleWidth);
+            CellExpression sampleCellExpression = new(sampleHeight, sampleWidth, true, cellStateStrings);
+            Board sampleBoard = CellExpression.GenerateBoard(sampleCellExpression);
 
-    private static int Msb(int n) {
-        if (n == 0) {
-            return -1;
+            Debug.Log(BoardHelper.GetBoardVisualization(sampleBoard));
         }
-        int msb = 0;
-        while (n != 0)
+        catch (Exception e)
         {
-            n >>= 1;
-            msb++;
+            Debug.LogError($"Error during board generation: {e.Message}");
+            Debug.LogException(e);
         }
-        return msb;
     }
 }
